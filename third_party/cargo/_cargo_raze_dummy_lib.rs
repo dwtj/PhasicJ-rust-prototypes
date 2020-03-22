8db5dbb423224aab1dfaf068fad25cb387cd93ae
David Johnston <dwtj@dwtj.me>
2020-03-22 15:17:21 -0700

    Add `third_party/cargo` to root Cargo workspace
    This commit makes the dummy `cargo-raze` Rust library part of the root
    Cargo workspace. This way, when the root Cargo workspace is built using
    Cargo, it knows about any dependency information specified in
    `third_party/cargo/Cargo.toml`.

---
+++ third_party/cargo/_cargo_raze_dummy_lib.rs
@@ -0,0 +1,5 @@
+// This empty Rust library exists to support `cargo-raze`. The `cargo-raze` tool
+// is used to make cargo dependencies available to Bazel targets. It is
+// implemented as a cargo subcommand and it is configured using a `Cargo.toml`
+// file. This `Cargo.toml` conventionally declares a "fake" Rust library. This
+// empty Rust file is that "fake" Rust library.
